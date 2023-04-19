981cbcf0f4184b4bc1d8479f847903cb53777a95
Tenali Sumanth <tsumanth171@gmail.com>
2023-04-19 12:32:28 +0530

    first commit

---
+++ .vscode/javascript/script.js
@@ -0,0 +1,43 @@
+//var x =prompt('enter day')
+
+switch(x){
+    case 'monday' :
+        document.write('class start at 8:30')
+        break;
+
+        case 'tuesday':
+            case 'wesnesday':
+                case 'thursday':
+                case 'friday': 
+                    case 'saturday':
+                        document.write(class start at 8:45am)
+                        break;
+
+                        case 'wesneday':
+                            document.write(class start at 8:45am)
+                            break;
+                            default(document.write)
+                            
+}
+ 
+ var grade =parsefloat prompt('enter marks')
+
+ switch(true){
+
+    case grade<=90;
+    document.write('a grade')
+    break;
+    case grade<=80;
+    document.write('b grade')
+    break;
+    case grade <=70;
+    document.write('c grade')
+    break;
+    case grade <=60;
+    document.write('d grade')
+    break;
+    case grade <=50;
+    document.write('f grade')
+    break;
+
+ }
